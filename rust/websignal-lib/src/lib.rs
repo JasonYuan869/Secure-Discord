cf219c8b01d0a1529f68bf551faadb90b2907f47
Jason Yuan <jason.yuan869@gmail.com>
2023-11-29 19:25:22 -0500

    Begin rust wrapper implementation

--- rust/websignal-lib/src/lib.rs
+++ rust/websignal-lib/src/lib.rs
@@ -1,9 +1,40 @@
 mod utils;
 
-use libsignal_protocol::kem::{KeyPair, KeyType};
+use libsignal_protocol::{
+    IdentityKeyPair,
+    PrivateKey,
+    PublicKey,
+    error::Result,
+};
+
+use rand::rngs::OsRng;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn init() {
     utils::set_panic_hook();
 }
+
+#[wasm_bindgen]
+pub struct Identity {
+    identity_key: IdentityKeyPair,
+}
+
+impl Identity {
+    pub fn new() -> Self {
+        Identity {
+            identity_key: IdentityKeyPair::generate(&mut OsRng)
+        }
+    }
+
+    pub fn serialize(&self) -> Box<[u8]> {
+        self.identity_key.serialize()
+    }
+
+    pub fn deserialize(value: &[u8]) -> Result<Self> {
+        let identity_key = IdentityKeyPair::try_from(value)?;
+        Ok(Identity { identity_key })
+    }
+}
+
+
